f72f7a43f8144901a7bfb7ab38de70a16749742a
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-24 22:53:52 +0600

    Update

--- loop.js
+++ loop.js
@@ -11,3 +11,10 @@ for(const prop in mobile){
     console.log(prop)
     console.log(mobile[prop])
 }
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    console.log(key, ':', mobile[key]);
+}
